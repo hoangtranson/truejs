d516279cda7c4cbea471d341a989ff7e052d9da0
Hoang Tran <hayden.tran.d@gmail.com>
2019-01-15 22:07:57 +0700

    test: 💍 add todo for testing isNaN function

--- tests/number.spec.js
+++ tests/number.spec.js
@@ -1,5 +1,5 @@
 import test from "ava";
-const { min, isNumber } = require("../number");
+const { min, isNumber, isNaN } = require("../number");
 
 test.todo("min should be a function");
 test.todo("min should throw error when argument is invalid");
@@ -10,3 +10,7 @@ test.todo("isNumber should be a function");
 test.todo("isNumber should throw error when argument is invalid");
 test.todo("isNumber should return valid number");
 test.todo("isNumber should return invalid number");
+
+test.todo("isNaN should be a function");
+test.todo("isNaN should return true");
+test.todo("isNaN should return false");
