2c0cf5d18439cb5731503e55da2ae6225a98727c
Hoang Tran <hayden.tran.d@gmail.com>
2019-01-15 22:03:14 +0700

    test: 💍 add todo for testing isNumber function

--- tests/number.spec.js
+++ tests/number.spec.js
@@ -1,7 +1,12 @@
 import test from "ava";
-const { min } = require("../number");
+const { min, isNumber } = require("../number");
 
 test.todo("min should be a function");
 test.todo("min should throw error when argument is invalid");
 test.todo("min should should return message [is not less than]");
 test.todo("min should should return message [is less than]");
+
+test.todo("isNumber should be a function");
+test.todo("isNumber should throw error when argument is invalid");
+test.todo("isNumber should return valid number");
+test.todo("isNumber should return invalid number");
