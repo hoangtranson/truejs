9e5657da4a29d2d5363095d15f96896ad43592aa
Hoang Tran Son <hayden.tran.d@gmail.com>
2019-02-08 21:29:47 +0700

    add function prep for string

---
+++ string/index.js
@@ -0,0 +1,31 @@
+
+const stringValidator = () => {
+    const isUppercase = () => value => value === value.toUpperCase();
+    const startWith = () => '';
+    const isOneOf = () => '';
+    const isNotEmpty = () => '';
+    const min = () => '';
+    const max = () => '';
+    const isMatch = () => '';
+    const isLowerCase = () => '';
+    const length = () => '';
+    const isString = () => '';
+    const isJson = () => '';
+    const isHtml = () => '';
+    const isHexColor = () => '';
+    const eq = () => '';
+    const extendedAscii = () => '';
+    const endWith = () => '';
+    const empty = () => '';
+    const contain = () => '';
+    const between = () => '';
+    const ascii = () => '';
+    const base64 = () => '';
+    const isMarkdown = () => '';
+
+    return {
+        isUppercase,
+    };
+};
+
+module.exports = stringValidator();
