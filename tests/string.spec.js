f2ada74e98b54b57eb5d914a32b89cb60558ace2
Hoang Tran Son <hayden.tran.d@gmail.com>
2019-02-08 21:38:35 +0700

    refactor(add unit test file for string):

---
+++ tests/string.spec.js
@@ -0,0 +1,7 @@
+import test from "ava";
+const {
+    isUppercase
+} = require("../string");
+
+test.todo("isUppercase should return true value");
+test.todo("isUppercase should return false value");
