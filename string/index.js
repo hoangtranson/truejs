dde20aa35cc1bbbdbaca203f57fe627afafec629
Hoang Tran Son <hayden.tran.d@gmail.com>
2019-02-13 21:54:09 +0700

    feat(string): add function base64

--- string/index.js
+++ string/index.js
@@ -48,7 +48,30 @@ const stringValidator = () => {
         const asciiMatcher = match(/^[\x00-\x7F]*$/);
         return value => asciiMatcher(value);
     };
-    const base64 = () => '';
+    const base64 = () => {
+        const base64Matcher = match(/^[A-Za-z0-9+/=]*$/);
+        return value => {
+            if (!base64Matcher(value)) {
+                return false;
+            }
+
+            if (value.length % 4 !== 0) {
+                return false;
+            }
+
+            const firstEqual = value.indexOf('=');
+
+            if (firstEqual > -1 && firstEqual < value.length - 2) {
+                return false;
+            }
+
+            if (firstEqual === value.length - 2 && !value.endsWith('=')) {
+                return false;
+            }
+
+            return true;
+        };
+    };
     const isMarkdown = () => '';
 
     return {
